8e8f57a52dcc80a1c231ac597a957c85ea4ab22a
Santiago Cifuentes <cifuentess938@gmail.com>
2023-02-03 11:26:01 -0500

    resolved errors on scrollreveal, adding visibility hidden y setTimeout

--- js/index.js
+++ js/index.js
@@ -1,98 +1,101 @@
 window.sr =ScrollReveal();
-
-    sr.reveal('#header',{
-        duration: 2000,
-    });
-
-    // ************Animations nav*************
-    sr.reveal('.logo',{
-        duration: 2500,
-        origin:'bottom',
-        distance: '-100px'
-    });
-    sr.reveal('.menu',{
-        duration: 2500,
-        origin:'bottom',
-        distance: '-100px'
-    });
-
-    sr.reveal('.header-text',{
-        duration: 2500,
-        origin:'left',
-        distance: '-100px'
-    });
-    // ************Animations About*************
-    sr.reveal('.about-col-1',{
-        duration: 2500,
-        origin:'right',
-        distance: '-100px'
-    });
-    sr.reveal('.about-col-2',{
-        duration: 2800,
-        origin:'right',
-        distance: '-100px'
-    });
-
-// ************Animations Skills*************
-
-    sr.reveal('.sub-title.sub2',{
-        duration: 3000,
-        origin:'bottom',
-        distance: '-50px'
-    });
-    sr.reveal('.list-icons',{
-        duration: 3500,
-        origin:'bottom',
-        distance: '-50px'
-    });
-    sr.reveal('.p2',{
-        duration: 3500,
-        origin:'bottom',
-        distance: '-50px'
-    });
-
-// ************Animations Services*************
-
-    sr.reveal('.sub-title.sub3',{
-        duration: 3000,
-        origin:'bottom',
-        distance: '-50px'
-    });
-    sr.reveal('.services-list',{
-        duration: 3000,
-        origin:'rigth',
-        distance: '100px'
-    });
-
-// ************Animations Porfolio*************
-
-    sr.reveal('.work-list',{
-        duration: 3000,
-        origin:'right',
-        distance: '-100px'
-    });
-    sr.reveal('.btn.a',{
-        duration: 3500,
-        origin:'top',
-        distance: '-30px'
-    });
-
-// ************Animations Contact*************
-
-    sr.reveal('.contact-left',{
-        duration: 3800,
-        origin:'bottom',
-        distance: '-100px'
-    });
-
-    sr.reveal('.contact-right',{
-        duration: 3800,
-        origin:'bottom',
-        distance: '-100px'
-    });
-
-    sr.reveal('.btn.btn2',{
-        duration: 4300,
-        // origin:'bottom',
-        // // distance: '-20px'
-    });
+    setTimeout(()=>{
+        sr.reveal('#header',{
+            duration: 2000,
+        });
+    
+        // ************Animations nav*************
+        sr.reveal('.logo',{
+            duration: 2500,
+            origin:'bottom',
+            distance: '-100px'
+        });
+        sr.reveal('.menu',{
+            duration: 2500,
+            origin:'bottom',
+            distance: '-100px'
+        });
+    
+        sr.reveal('.header-text',{
+            duration: 2500,
+            origin:'left',
+            distance: '-100px'
+        });
+        // ************Animations About*************
+        sr.reveal('.about-col-1',{
+            duration: 2500,
+            origin:'right',
+            distance: '-100px'
+        });
+        sr.reveal('.about-col-2',{
+            duration: 2800,
+            origin:'right',
+            distance: '-100px'
+        });
+    
+    // ************Animations Skills*************
+    
+        sr.reveal('.sub-title.sub2',{
+            duration: 3000,
+            origin:'bottom',
+            distance: '-50px'
+        });
+        sr.reveal('.list-icons',{
+            duration: 3500,
+            origin:'bottom',
+            distance: '-50px'
+        });
+        sr.reveal('.p2',{
+            duration: 3500,
+            origin:'bottom',
+            distance: '-50px'
+        });
+    
+    // ************Animations Services*************
+        
+        sr.reveal('.services-list',{
+            duration: 3000,
+            origin:'rigth',
+            distance: '100px'
+        });
+        sr.reveal('.sub-title.sub3',{
+            duration: 3000,
+            origin:'bottom',
+            distance: '-50px'
+        });
+        
+    
+    // ************Animations Porfolio*************
+    
+        sr.reveal('.work-list',{
+            duration: 3000,
+            origin:'right',
+            distance: '-100px'
+        });
+        sr.reveal('.btn.a',{
+            duration: 3500,
+            origin:'top',
+            distance: '-30px'
+        });
+    
+    // ************Animations Contact*************
+    
+        sr.reveal('.contact-left',{
+            duration: 3800,
+            origin:'bottom',
+            distance: '-100px'
+        });
+    
+        sr.reveal('.contact-right',{
+            duration: 3800,
+            origin:'bottom',
+            distance: '-100px'
+        });
+    
+        sr.reveal('.btn.btn2',{
+            duration: 4300,
+            // origin:'bottom',
+            // // distance: '-20px'
+        });
+    },1000);
+    
